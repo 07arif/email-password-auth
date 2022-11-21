d38daa4adda852da8ab3921daa2456e806e0e5a2
07arif <ai.arif0711@gmail.com>
2022-11-21 20:32:07 +0600

    onchange

--- src/App.js
+++ src/App.js
@@ -10,16 +10,24 @@ const handleRegister = (event) => {
   const email = event.target.email.value;
   const password = event.target.password.value;
   console.log(email, password);
-  
+}
+
+const handleEmailChange = event => {
+  console.log(event.target.value)
+}
+const handlePasswordChange = event =>{
+  const pass= event.target.value
+  const passLength = pass.length
+  console.log(pass,passLength)
 }
 
 function App() {
   return (
     <div className="App">
       <form onSubmit={handleRegister}>
-        <input type="email" name="email" id="" placeholder='Your Email' />
+        <input onChange={handleEmailChange} type="email" name="email" id="" placeholder='Your Email' />
         <br />
-        <input type="password" name="password" id="" placeholder='Your Password' />
+        <input onChange={handlePasswordChange} type="password" name="password" id="" placeholder='Your Password' />
         <br />
         <button type="submit">Register</button>
       </form>
