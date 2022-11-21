e144f518debfbdecaf25247b748514fa50556cfd
07arif <ai.arif0711@gmail.com>
2022-11-21 20:13:41 +0600

    collect value from form

---
+++ src/firebase/_steps.js
@@ -0,0 +1,14 @@
+/*
+Auth Terminology:
+-------------------
+"Register" or "Sign Up" [First time create a user]
+"log in" or "sign in" [Already you have a user account created]
+
+*/
+
+/*
+        get form value:
+        set form onsubmithandler
+        set event parameter
+        event.target.email.value
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALdOVVjnA8N6TOXe8vfoWHKkpF0BEje-k",
+  authDomain: "email-password-auth-c986d.firebaseapp.com",
+  projectId: "email-password-auth-c986d",
+  storageBucket: "email-password-auth-c986d.appspot.com",
+  messagingSenderId: "384569189758",
+  appId: "1:384569189758:web:854eab4cf5ee3c29587f2f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
