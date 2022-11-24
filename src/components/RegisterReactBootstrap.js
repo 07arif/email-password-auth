d28fc2b6c6184ea63ff0bbc8be01557ce44c0790
07arif <ai.arif0711@gmail.com>
2022-11-24 21:03:11 +0600

    rgex

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import{createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 import app from '../firebase/firebase.init'
 
-
 const auth = getAuth(app);
 
 const RegisterReactBootstrap = () => {
+    const [passwordError, setPasswordError]=useState('')
     const handleRegister = event => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value
         console.log(email, password);
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+            setPasswordError('Please add at least two uppercase')
+            return;
+        }
+        if(password.length < 6){
+            setPasswordError('Password should be 6 character')
+            return;
+        }
+        if(!/(?=.*[!@#$&*])/.test(password)){
+            setPasswordError('Please add at least one special character');
+            return;
+        }
+        setPasswordError('')
         createUserWithEmailAndPassword(auth,email,password)
         .then(result=>{
             const user = result.user
@@ -38,6 +51,7 @@ const RegisterReactBootstrap = () => {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 </Form.Group>
+                <p className='text-danger'>{passwordError}</p>
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
